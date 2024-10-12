7543b8c4333bd9e7796f6688be47b39fbab9c4ec
amir-ramezany <ramezaniamir47@gmail.com>
2024-10-13 00:27:11 +0330

    dev amir code

--- new-branch.js
+++ new-branch.js
@@ -1,7 +1,9 @@
 function newBranch() {
   return <h1>new Branch</h1>;
 }
-
 alert("new branch");
-console.log("add from outside") 
-
+console.log("add from outside");
+/*  
+its something that dev-amir commited  
+*/
+const devAmir = "devAmir";
