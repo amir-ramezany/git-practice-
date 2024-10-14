b2e81cd5d0a4fb3ce85916f9d8e4da5c0acc2c93
amir-ramezany <ramezaniamir47@gmail.com>
2024-10-14 11:33:41 +0330

    add number variable

--- addSomething.js
+++ addSomething.js
@@ -3,3 +3,6 @@ function addSomething() {
   return console.log(288 * 5, num);
 }
 /* this is new comment  */
+const number = 56;
+if (number > 60) return <div>return--somthing</div>;
+/* this is the number constant */
